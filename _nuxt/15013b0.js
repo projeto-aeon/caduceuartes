284033918dfc58a04283616c2ed82cb22304619a
AlbertoAlfredo <raioalfredo@gmail.com>
2021-07-31 22:02:56 -0300

    Updates

---
+++ _nuxt/15013b0.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[10],{242:function(e,n,t){"use strict";t.r(n);var l=t(1).a.extend({}),c=t(10),component=Object(c.a)(l,(function(){var e=this,n=e.$createElement;return(e._self._c||n)("div",[e._v("\n  teste\n")])}),[],!1,null,null,null);n.default=component.exports}}]);
