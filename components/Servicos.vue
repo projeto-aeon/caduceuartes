2416cfed42d81a36480fca1796f8c695858efabd
Alberto <raioalfredo@gmail.com>
2021-10-21 23:20:29 -0300

    atualizacoes

--- components/Servicos.vue
+++ components/Servicos.vue
@@ -5,7 +5,7 @@
   <div style="display: flex; flex-direction: row; justify-content: space-between;" >
   <div style="padding: 2vw;" v-for="(servico, index) in this.servicos" :key="index">
     <div>
-    <a :href="servico.link">
+    <a :href="servico.link" target="_blank">
     <h1 class="z-50" style="width: 10vw; margin-bottom: -8vw; position: relative; color: var(--thirdy-color); height: 10vw;"><b>{{servico.nome}}</b></h1>
     <nuxt-img class="z-10" style="width: 20vw; height: 20vw; border-radius: 1vw; position: relative;" :src="servico.img" />
     </a>
@@ -21,7 +21,7 @@ export default {
   data(){
     return{
     servicos:[
-      {nome: "Eyevision Mídias Interativas", link: "http://eyevision.ai/", img: "/clientes/eyevisionprint.jpg"},
+      {nome: "Reporter Lian Lucas", link: "http://reporterlianlucas.com.br/", img: "/clientes/reporter-lian-lucas.png"},
       {nome: "Blog - Onsifer", link: "http://onsifer.com.br/", img: "/clientes/onsifer.png"},
     ]
     }
